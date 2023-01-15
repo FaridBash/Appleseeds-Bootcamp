5940f954a5f80eef1880fc42cde32b2ba27ced60
FaridBash <farid.bashiti@gmail.com>
2023-01-15 13:07:29 +0200

    while

---
+++ Javascript/for loops/92 while loop/app.js
@@ -0,0 +1,16 @@
+
+
+let arr=["Farid", "fari", "far", "fa","f"];
+
+const arrLen=[];
+let i=0;
+
+while(i<arr.length){
+    let str=arr[i];
+    arrLen.push((str.length));
+    i++;
+
+}
+
+
+console.log(arrLen);
