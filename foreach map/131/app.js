6e0dab936758de7c3ae4b5b300a6e1d88f436c53
FaridBash <farid.bashiti@gmail.com>
2023-01-17 11:01:17 +0200

    131

---
+++ foreach map/131/app.js
@@ -0,0 +1,13 @@
+
+
+const numbers = [1, -5, 666, 2, 400, 11];
+
+const asc=numbers.sort((a,b)=> a-b);
+
+console.log(asc);
+console.log('=====================');
+
+
+const desc=numbers.sort((a,b)=> b-a);
+
+console.log(asc);
