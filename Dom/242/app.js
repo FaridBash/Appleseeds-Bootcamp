6abd510026762097dc9eef07d5fab899349bdaa3
FaridBash <farid.bashiti@gmail.com>
2023-01-31 11:31:40 +0200

    pokemon

---
+++ Dom/242/app.js
@@ -0,0 +1,37 @@
+function Pokemon(pokemonName, pokemonType,
+    pokemonAttackList){
+    this.name = pokemonName;
+    this.type = pokemonType;
+    this.attackList = pokemonAttackList;
+    }
+
+const pikatchu=new Pokemon('pikatchu', 'electric', ['elec','punch']);
+const charmandar=new Pokemon('charmandar', 'fire', ['fireAttach','tail']);
+const squirel=new Pokemon('squirel', 'water', ['puching','waterAttach']);
+
+
+Pokemon.prototype.callPokemon=function(){
+    console.log(`I call you, ${(this.name)}`);
+}
+
+Pokemon.prototype.attack=function(num){
+    console.log(`${(this.name)} used ${(this.attackList[num])}`);
+}
+
+
+pikatchu.callPokemon();
+pikatchu.attack(0);
+console.log('==========================');
+charmandar.callPokemon();
+charmandar.attack(1);
+console.log('==========================');
+squirel.callPokemon();
+squirel.attack(0);
+console.log('==========================');
+// console.log(pikatchu.callPokemon());
+// console.log(pikatchu.attack(1));
+// console.log(squirel.callPokemon());
+// console.log(squirel.attack(1));
+// console.log(charmandar.callPokemon());
+// console.log(charmandar.attack(0));
+
