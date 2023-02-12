5c7aff7d66af5869ae7caa9d854ff460b822bfe1
FaridBash <farid.bashiti@gmail.com>
2023-02-12 14:34:00 +0200

    tikon

--- React/Day1/3.1/vite 31/targeel31/src/App.jsx
+++ React/Day1/3.1/vite 31/targeel31/src/App.jsx
@@ -1,6 +1,10 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import HelloWorld from './cmponents/HelloWorld'
+import Plus from './cmponents/plus'
+import StringLength from './cmponents/Length'
+
 // import HelloWorld from './components'
 
 function App() {
@@ -9,8 +13,12 @@ function App() {
   return (
     <div className="App"> 
       <h1>AppleSeeds React App</h1>
+      <HelloWorld />
+    <Plus />
+    <StringLength />
     </div>
   )
 }
+
 
 export default App
--- React/Day1/3.1/vite 31/targeel31/src/main.jsx
+++ React/Day1/3.1/vite 31/targeel31/src/main.jsx
@@ -1,16 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import HelloWorld from './cmponents/HelloWorld'
-import Plus from './cmponents/plus'
-import StringLength from './cmponents/Length'
+
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <HelloWorld />
-    <Plus />
-    <StringLength />
   </React.StrictMode>,
 )
