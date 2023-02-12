05c795d61c1ac83dff8b89f7f2193c183baa52d4
FaridBash <farid.bashiti@gmail.com>
2023-02-12 14:06:35 +0200

    targeel32 part 3

---
+++ React/Day1/3.1/vite 31/targeel31/src/cmponents/Length.jsx
@@ -0,0 +1,10 @@
+
+
+function StringLength(){
+    const string = "I love React!"
+
+    return `The string’s length is${string.length}`;
+}
+
+
+export default StringLength;
--- React/Day1/3.1/vite 31/targeel31/src/main.jsx
+++ React/Day1/3.1/vite 31/targeel31/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import HelloWorld from './cmponents/HelloWorld'
 import Plus from './cmponents/plus'
+import StringLength from './cmponents/Length'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -10,5 +11,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
     <HelloWorld />
     <Plus />
+    <StringLength />
   </React.StrictMode>,
 )
