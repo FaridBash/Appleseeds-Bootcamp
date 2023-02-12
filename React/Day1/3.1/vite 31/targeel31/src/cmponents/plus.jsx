f8d652ade3afc7bef72bf5f0c107564808ee47ce
FaridBash <farid.bashiti@gmail.com>
2023-02-12 14:02:51 +0200

    targeel32 part 2

---
+++ React/Day1/3.1/vite 31/targeel31/src/cmponents/plus.jsx
@@ -0,0 +1,10 @@
+
+
+function Plus(){
+    const number1 = 5;
+    const number2 =6;
+
+    return <h1>{(number1+number2)}</h1>
+}
+
+export default Plus;
--- React/Day1/3.1/vite 31/targeel31/src/main.jsx
+++ React/Day1/3.1/vite 31/targeel31/src/main.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import HelloWorld from './cmponents/HelloWorld'
+import Plus from './cmponents/plus'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
     <HelloWorld />
+    <Plus />
   </React.StrictMode>,
 )
