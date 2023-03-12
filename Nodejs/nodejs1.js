6d9719a1b3b48f62810e905ffaeae9e0ade7d8d1
FaridBash <farid.bashiti@gmail.com>
2023-03-12 11:43:54 +0200

    node 1

---
+++ Nodejs/nodejs1.js
@@ -0,0 +1,9 @@
+
+// global: node doesnt need window so it keeps its work in the global
+// ablitiy to teach js new things
+// access file system: node can access file systems through c++ code from the v8 engine
+
+
+
+// document DOM can do staff with the elements 
+//window it has to have window so it can work
